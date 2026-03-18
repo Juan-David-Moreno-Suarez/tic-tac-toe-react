d58322f42bec5345179c00960e6a172b3301891b
Juan-David-Moreno-Suarez <juanmorsua@unisabana.edu.co>
2026-03-18 06:28:59 -0500

    Turnos implementados

--- src/App.js
+++ src/App.js
@@ -1,3 +1,51 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+
+  return <button
+    className="square"
+    onClick={onSquareClick}
+  >
+    {value}
+  </button>
 }
+
+function Row({ n, index, value, onSquareClick }) {
+  const items = Array.from({ length: n });
+  return (
+    <section className="board-row">
+      {
+        items.map((_, i) => (<Square className="square" value={value[i]} onSquareClick={() => onSquareClick(n * index + i)} />))
+      }
+    </section>
+  );
+}
+
+export default function Board() {
+  const n = 4;
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(n * n).fill(null));
+  const items = Array(n).fill(null);
+
+  function handleClick(i) {
+    if (squares[i]) return;
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    }
+    else {
+      nextSquares[i] = "O";
+    }
+    setXIsNext(!xIsNext);
+    setSquares(nextSquares);
+  }
+  return (
+    <>
+      {
+        items.map((_, i) => (<Row n={n} index={i} value={squares.slice(n * i, n * i + n)} onSquareClick={handleClick} />))
+      }
+    </>
+  );
+}
+
+function calculateWinner(squares, n) { }
